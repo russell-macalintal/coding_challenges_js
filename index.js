27f2742acbf1c6f73e106d22867c6a205bd1ae8b
rumacalin <russell.macalintal@gmail.com>
2022-01-14 18:53:46 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// SAMPLE CODE TO ENSURE INDEX.JS FUNCTIONALITY
+const greet = (name = "Tester") => {
+    console.log(`Hello ${name}`);
+}
+
+greet();
+
+
+// JPMCC CODING CHALLENGE
+// Input: jumbled numeral characters
+// Output: numerical representations of jumbled words sorted in ascending order
+// Import: Duplicates are not excluded from the output
