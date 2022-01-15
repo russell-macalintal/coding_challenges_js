51eb0d2f473e3218e24ed55fd50f3df0f210644f
rumacalin <russell.macalintal@gmail.com>
2022-01-14 19:07:31 -0500

    Write word scrambler function

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 // SAMPLE CODE TO ENSURE INDEX.JS FUNCTIONALITY
-const greet = (name = "Tester") => {
-    console.log(`Hello ${name}`);
-}
+// const greet = (name = "Tester") => {
+//     console.log(`Hello ${name}`);
+// }
 
-greet();
+// greet();
 
 
 // JPMCC CODING CHALLENGE
 // Input: jumbled numeral characters
 // Output: numerical representations of jumbled words sorted in ascending order
-// Import: Duplicates are not excluded from the output
+// Import: Duplicates are not excluded from the outputw
+
+const scrambler = (word) => {
+    let a = word.split("");
+    let len = a.length;
+
+    for(let i = len - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+    
+    return a.join("");
+}
+
+console.log(scrambler("zeroonetwoeightnine"));
